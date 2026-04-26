2629dbec76f08e587e77a8e0dde7096808715127
ofskydavid <ofskydavid@gmail.com>
2026-04-26 17:35:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from flask import Flask
 # ================= НАСТРОЙКИ =================
 TELEGRAM_TOKEN = "8620473509:AAFa8BIAUuH5IU8GDrFGz4pn5EGbzvcFZ90"
 TELEGRAM_USER_ID = "478140816"
-KUFAR_URL = "https://www.kufar.by/l/r~vitebsk/mobilnye-telefony/mt~android?oph=1&prc=r%3A0%2C50000&sort=lst.d"
+KUFAR_URL = "https://www.kufar.by/l/mobilnye-telefony?oph=1&pos=v.or%3A1%2C5&prc=r%3A0%2C300000&sort=lst.d"
 CHECK_INTERVAL = 3          # минуты между проверками
 SELF_URL = os.environ.get("RENDER_EXTERNAL_URL", "https://kufar-monitor.onrender.com")
 # =============================================
